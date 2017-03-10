9a105ae3ed869045e70aa3301d893e27ae629b1c
Kamran Ahmed Ansari <team.radiant@gmail.com>
2017-03-10 19:18:20 +0530

    Add support for slides in all the pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,49 @@
 $().ready(function() {
+    var lastSlideIndex = +$("[data-slide]:last").attr("data-slide");
+
+    var currentSlide = getCurrentSlideNumber();
+
+    $("[data-slide]").hide();
+
+    $("body").keydown(function(event) {
+        switch(event.keyCode) {
+            case 37:
+                previousSlide();
+                break;
+            case 39:
+                nextSlide();
+                break;
+        }
+    });
+
+    changePage(currentSlide);
     
+    function changePage(slideNumber) {
+        window.location.href = window.location.href.split("#")[0] + "#" + slideNumber;
+
+        currentSlide = slideNumber;
+
+        $("[data-slide]").hide();
+        $("[data-slide=" + slideNumber + "]").fadeIn();
+    }
+
+    function getCurrentSlideNumber() {
+        return window.location.href.split("#")[1] != undefined ? +(window.location.href.split("#")[1]) : 1;
+    }
+
+    function previousSlide() {
+        currentSlide--;
+        
+        if (currentSlide < 1) currentSlide = 1;
+        
+        changePage(currentSlide);
+    };
+    
+    function nextSlide() {
+        currentSlide++;
+        
+        if (currentSlide > lastSlideIndex) currentSlide = lastSlideIndex;
+        
+        changePage(currentSlide);
+    };
 });
